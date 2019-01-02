a865971595a2d4fc346c086a4a7a1f85c9fe5996
mr-fool <mr-fool@users.noreply.github.com>
2019-01-02 12:47:13 -0700

    Update OurFirstSmartContract.sol

--- OurFirstSmartContract.sol
+++ OurFirstSmartContract.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.25 <0.6.0;
 
 contract OurFirstSmartContract {
-  uint256 myCounter;
+  uint256 public myCounter;
   
   function setMyCounter(uint _myCounter) public {
       myCounter = _myCounter;

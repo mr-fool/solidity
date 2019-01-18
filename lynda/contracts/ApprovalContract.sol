7a7383580a707768285b6028a1e82f33aabbbc07
mr-fool <mr-fool@users.noreply.github.com>
2019-01-17 22:31:37 -0700

    tutorial exercise

---
+++ lynda/contracts/ApprovalContract.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.4.18 <0.6.0;
+
+contract ApprovalContract {
+	address public sender;
+	address public receiver;
+	address public constant approver = 0xBB3DcC3a4385A6B20d9A6b8bda47732adD978aF2;
+
+	function deposit(address _receiver) external payable {
+		require(msg.value > 0);
+		sender = msg.sender;
+		receiver = _receiver;
+	}
+
+	function viewApprover() external pure returns(address) {
+		return(approver);
+	}
+
+	function approve() external {
+		require(msg.sender == approver);
+		receiver.transfer(address(this).balance);
+
+	}
+}

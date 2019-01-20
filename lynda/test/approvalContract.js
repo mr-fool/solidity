ea3f5bb408ed07b6dd92c4a7a13ef25690632e4b
mr-fool <mr-fool@users.noreply.github.com>
2019-01-20 15:52:54 -0700

    more change

--- lynda/test/approvalContract.js
+++ lynda/test/approvalContract.js
@@ -6,4 +6,9 @@ contract('ApprovalContract', function(accounts) {
         const approver = await contract.approver.call();
         assert.equal(approver,0xBB3DcC3a4385A6B20d9A6b8bda47732adD978aF2,"approvers don't match");
     });
+    it('takes a deposit', async function () {
+        const contract = await ApprovalContract.deployed();
+        await contract.deposit(accounts[0], { value: 1e+18, from: accounts[1] });
+        assert.equal(web3.eth.getBalance(contract.address), 1e+18, "amount did not match");
+      });
 })

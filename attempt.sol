dcc1b1ee73441d051435c3c926f4710840b04c1f
mr-fool <mr-fool@users.noreply.github.com>
2019-01-27 18:50:47 -0700

    updated for the new version of solidity

--- attempt.sol
+++ attempt.sol
@@ -1,16 +1,16 @@
-pragma solidity ^0.4.17;
+pragma solidity >=0.4.22 <0.6.0;
 
 contract Inbox {
     string public message;
     
     
-    function Inbox(string initialMessage) public {
+    constructor(string memory initialMessage) public {
         message = initialMessage;
     }
-    function setMessage(string newMessage) public {
+    function setMessage(string memory newMessage) public {
         message = newMessage;
     }
-    function getMessage() public view returns(string) {
+    function getMessage() public view returns(string memory) {
         return message;
     }
 }

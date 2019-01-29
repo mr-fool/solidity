0269dae9188c0826ecba238ca4c81f2755b507c4
mr-fool <mr-fool@users.noreply.github.com>
2019-01-28 18:59:17 -0700

    no need for getter

--- attempt.sol
+++ attempt.sol
@@ -10,7 +10,4 @@ contract Inbox {
     function setMessage(string memory newMessage) public {
         message = newMessage;
     }
-    function getMessage() public view returns(string memory) {
-        return message;
-    }
 }

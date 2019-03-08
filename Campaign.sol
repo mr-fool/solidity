45aa02a5ed131374bb19dd60a885ef003a2842a5
mr-fool <mr-fool@users.noreply.github.com>
2019-03-08 14:58:27 -0700

    Create Campaign.sol

---
+++ Campaign.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Campaign {
+    address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
+    
+    constructor(uint minimum) public {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers.push(msg.sender);
+    }
+}

747794ee75f1b49d729b184b6358986c432e7392
mr-fool <mr-fool@users.noreply.github.com>
2019-03-13 21:58:40 -0600

    Update Campaign.sol

--- Campaign.sol
+++ Campaign.sol
@@ -43,8 +43,11 @@ contract Campaign {
         requests.push(newRequest);
     }
     function approveRequest(uint index) public {
+        Request storage request = requests[index];
         require(approvers[msg.sender]);
-        require(!requests[index].approvals[msg.sender]);
+        require(!request.approvals[msg.sender]);
         
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
     }
 }
